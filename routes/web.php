7a6344478b45c46338eb242524af57f1e62fd59c
Dilovar Azimov <dilovar09@gmail.com>
2021-02-04 18:21:43 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,11 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-//Route::get('{all}', function () {
+Route::get('/videos', function () {
     //return file_get_contents(public_path('_nuxt/index.html'));
-    //return abort(404);
-//})->where('all', '^(?!videos).*$');
+    return public_path('/videos');
+})->where('all', '^(?!api).*$');
+Route::get('{all}', function () {
+    //return file_get_contents(public_path('_nuxt/index.html'));
+    return abort(404);
+})->where('all', '^(?!api).*$');
